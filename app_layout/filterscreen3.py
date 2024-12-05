9cba42c3de6e137422dec3a5bd428ee388bc3f38
Dolan <sdolazinski@gmail.com>
2024-12-05 16:27:11 +0100

    filterscreen3 added: backdend to date and price Filters

--- app_layout/filterscreen3.py
+++ app_layout/filterscreen3.py
@@ -5,9 +5,12 @@ from kivy.metrics import dp
 
 from kivymd.uix.appbar import MDBottomAppBar
 from kivymd.uix.floatlayout import MDFloatLayout
+from kivymd.uix.gridlayout import MDGridLayout
 from kivymd.uix.menu import MDDropdownMenu
 from kivymd.uix.pickers import MDDockedDatePicker
-from kivymd.uix.snackbar import MDSnackbar, MDSnackbarSupportingText, MDSnackbarText
+from kivymd.uix.selectioncontrol import MDCheckbox
+from kivy.uix.checkbox import CheckBox
+from kivymd.uix.textfield import MDTextField
 
 from app_layout.just_screen import JustScreen
 from kivy.lang import Builder
@@ -26,7 +29,8 @@ FS3 = """
         theme_bg_color: "Custom"
         md_bg_color: "#4267B2"
         id: top_appbar
-        pos_hint: {"center_x": .5, "center_y": 0.96}
+        size_hint: 1, .1
+        pos_hint: {"center_x": .5, "center_y": 0.95}
         
         MDTopAppBarLeadingButtonContainer:
     
@@ -37,12 +41,16 @@ FS3 = """
         MDTopAppBarTitle:
             text: "Filters"
             
-    MDRecycleView:
+    # MDRecycleView:
+    #     padding: [80, 0, 80, 0]
+    #     size_hint_y: None
+    #     height: self.parent.height - dp(138)
+    #     pos_hint: {"center_x": .5, "center_y": 0.525}
+    ScrollView:
         padding: [80, 0, 80, 0]
-        size_hint_y: None
-        height: self.parent.height - dp(138)
-        pos_hint: {"center_x": .5, "center_y": 0.525}
-        
+        size_hint: 1, .8
+        # height: self.parent.height - dp(138)
+        pos_hint: {"center_x": .5, "center_y": 0.5}
         MDGridLayout:
             id: box
             cols: 1
@@ -103,9 +111,13 @@ FS3 = """
                 md_bg_color: "#4267B2"
                 adaptive_height: True
                 height: "300dp"
-                
-    BotAppBar:
-        id: bottom_appbar
+        
+    ButtonLayout3:
+        size_hint: 1, .1
+        theme_bg_color: "Custom"
+        md_bg_color: "#2367B2"
+    # BotAppBar:
+    #     id: bottom_appbar
 """
 
 
@@ -137,8 +149,6 @@ class FilterScreen3(JustScreen):
     def show_new_filter(self):
         pass
 
-    # TODO dokończyć ui, Odbiorcy, Sprzedawca, Sklep
-
 
 DF3 = """
 <DateFilter3>:
@@ -146,53 +156,56 @@ DF3 = """
         text: "Data zakupu"
         size_hint: None, None
         size: "100dp", "30dp"
-        pos_hint: {"x": 0.1, "y": 0.8}
-    
-    MDCheckbox:
-        group: "dates"
-        size_hint: None, None
-        size: "40dp", "40dp"
-        pos_hint: {"x": 0.1, "y": 0.65}
+        pos_hint: {"center_x": 0.1, "center_y": 0.9}
     
     MDLabel:
         text: "Option 1"
         size_hint: None, None
         size: "100dp", "30dp"
-        pos_hint: {"x": 0.2, "y": 0.65}
-
-    # Checkbox 2 with label
-    MDCheckbox:
-        group: "dates"
+        pos_hint: {"center_x": 0.2, "center_y": 0.75}
+    
+    CheckBox:
+        id: date_box
+        group: 'dates'
         size_hint: None, None
         size: "40dp", "40dp"
-        pos_hint: {"x": 0.1, "y": 0.45}
-
+        on_active: root.on_checkbox_active(*args)
+        pos_hint: {"center_x": 0.1, "center_y": 0.75}
+    
     MDLabel:
         text: "Option 2"
         size_hint: None, None
         size: "100dp", "30dp"
-        pos_hint: {"x": 0.2, "y": 0.45}
-
-    # Checkbox 3 with label
-    MDCheckbox:
-        group: "dates"
+        pos_hint: {"center_x": 0.2, "center_y": 0.55}
+        
+    CheckBox:
+        group: 'dates'
         size_hint: None, None
         size: "40dp", "40dp"
-        pos_hint: {"x": 0.1, "y": 0.25}
+        on_active: root.on_checkbox_active(*args)
+        pos_hint: {"center_x": 0.1, "center_y": 0.55}
+
     
     MDLabel:
         text: "Option 3"
         size_hint: None, None
         size: "100dp", "30dp"
-        pos_hint: {"x": 0.2, "y": 0.25}
+        pos_hint: {"center_x": 0.2, "center_y": 0.35}
+
+    CheckBox:
+        group: 'dates'
+        size_hint: None, None
+        size: "40dp", "40dp"
+        on_active: root.on_checkbox_active(*args)
+        pos_hint: {"center_x": 0.1, "center_y": 0.35}
 
     # DatePicker
     MDTextField:
-        id: field
+        id: field_from
         mode: "outlined"
         pos_hint: {'center_x': .25, 'center_y': .15}
         size_hint: .4, .1
-        on_focus: root.show_docked_date_picker(self.focus)
+        on_focus: root.show_docked_date_picker(self.focus, "from")
         # validator: "date"
         # date_format: "dd/mm/yyyy"
 
@@ -203,11 +216,11 @@ DF3 = """
             icon: "calendar"
    
     MDTextField:
-        id: field
+        id: field_to
         mode: "outlined"
         pos_hint: {'center_x': .75, 'center_y': .15}
         size_hint: .4, .1
-        on_focus: root.show_docked_date_picker(self.focus)
+        on_focus: root.show_docked_date_picker(self.focus, "to")
         # validator: "date"
         # date_format: "dd/mm/yyyy"
 
@@ -226,31 +239,46 @@ class DateFilter3(MDFloatLayout):
         super().__init__()
         self.size_hint = (1, .5)
 
-    def on_ok(self, instance_date_picker):
+    def on_checkbox_active(self, checkbox, value):
+        # xd = checkbox.get_widgets(groupname='dates')
+        # print(xd)
+        # print(type(xd))
+        if value:
+            self.clear_date_fields()
+        else:
+            print('The checkbox', checkbox, 'is inactive', 'and', checkbox.state, 'state')
+
+    def disable_checkboxes(self) -> None:
+        xd = self.ids.hello.get_widgets('dates')
+        for czek in xd:
+            czek.active = False
+
+    def clear_date_textfields(self) -> None:
+        if self.ids.field_from.text:
+            self.ids.field_from.text = ""
+        if self.ids.field_to.text:
+            self.ids.field_to.text = ""
+
+    def on_ok_from(self, instance_date_picker) -> None:
         date = instance_date_picker.get_date()
-        MDSnackbar(
-            MDSnackbarText(
-                text="Selected dates is:",
-            ),
-            MDSnackbarSupportingText(
-                text="\n".join(str(date) for date in instance_date_picker.get_date()),
-                padding=[ 0, 0, 0, dp(12) ],
-            ),
-            y=dp(124),
-            pos_hint={"center_x": 0.5},
-            size_hint_x=0.5,
-            padding=[ 0, 0, "8dp", "8dp" ],
-        ).open()
-        self.ids.field.text = str(date[ 0 ].strftime("%d/%m/%Y"))
+        self.ids.field_from.text = str(date[ 0 ].strftime("%d/%m/%Y"))
         instance_date_picker.dismiss()
 
-    def show_docked_date_picker(self, focus):
+    def on_ok_to(self, instance_date_picker) -> None:
+        date = instance_date_picker.get_date()
+        self.ids.field_to.text = str(date[ 0 ].strftime("%d/%m/%Y"))
+        instance_date_picker.dismiss()
+
+    def show_docked_date_picker(self, focus, from_or_to):
         if not focus:
             return
-
+        self.disable_checkboxes()
         date_dialog = MDDockedDatePicker()
         # You have to control the position of the date picker dialog yourself.
-        date_dialog.bind(on_ok=self.on_ok)
+        if from_or_to == "from":
+            date_dialog.bind(on_ok=self.on_ok_from)
+        else:
+            date_dialog.bind(on_ok=self.on_ok_to)
         date_dialog.open()
 
 
@@ -263,11 +291,13 @@ PF3 = """
         size: "100dp", "30dp"
         pos_hint: {"x": 0.1, "y": 0.8}
     
-    MDCheckbox:
-        group: "price"
+    CheckBox:
+        id: price_box
+        group: 'price'
         size_hint: None, None
         size: "40dp", "40dp"
         pos_hint: {"x": 0.1, "y": 0.65}
+        on_active: root.on_checkbox_active(*args)
     
     MDLabel:
         text: "do 100,00"
@@ -276,11 +306,12 @@ PF3 = """
         pos_hint: {"x": 0.2, "y": 0.65}
 
     # Checkbox 2 with label
-    MDCheckbox:
-        group: "price"
+    CheckBox:
+        group: 'price'
         size_hint: None, None
         size: "40dp", "40dp"
         pos_hint: {"x": 0.1, "y": 0.45}
+        on_active: root.on_checkbox_active(*args)
 
     MDLabel:
         text: "od 100,00 do 1000,00"
@@ -289,11 +320,12 @@ PF3 = """
         pos_hint: {"x": 0.2, "y": 0.45}
 
     # Checkbox 3 with label
-    MDCheckbox:
-        group: "price"
+    CheckBox:
+        group: 'price'
         size_hint: None, None
         size: "40dp", "40dp"
         pos_hint: {"x": 0.1, "y": 0.25}
+        on_active: root.on_checkbox_active(*args)
     
     MDLabel:
         text: "powyżej 1000,00"
@@ -303,11 +335,12 @@ PF3 = """
 
     # price picker
     MDTextField:
-        id: price_field
+        id: price_field_from
         mode: "outlined"
         pos_hint: {'center_x': .25, 'center_y': .15}
         size_hint: .4, .1
         text: ""
+        on_focus: root.is_text_ok(*args)
 
         MDTextFieldHintText:
             text: "Od"
@@ -316,11 +349,12 @@ PF3 = """
             icon: "numeric"
     
     MDTextField:
-        id: price_field
+        id: price_field_to
         mode: "outlined"
         pos_hint: {'center_x': .75, 'center_y': .15}
         size_hint: .4, .1
         text: ""
+        on_focus: root.is_text_ok(*args)
 
         MDTextFieldHintText:
             text: "Do"
@@ -337,10 +371,56 @@ class PriceFilter3(MDFloatLayout):
         super().__init__()
         self.size_hint = (1, .5)
 
+    def on_checkbox_active(self, checkbox, value):
+        # xd = checkbox.get_widgets(groupname='dates')
+        # print(xd)
+        # print(type(xd))
+        if value:
+            self.clear_price_textfields()
+        else:
+            print('The checkbox', checkbox, 'is inactive', 'and', checkbox.state, 'state')
+
+    def disable_checkboxes(self) -> None:
+        xd = self.ids.price_box.get_widgets('price')
+        for czek in xd:
+            czek.active = False
+
+    def clear_price_textfields(self) -> None:
+        if self.ids.price_field_from.text:
+            self.ids.price_field_from.text = ""
+        if self.ids.price_field_to.text:
+            self.ids.price_field_to.text = ""
+
+    def is_text_ok(self, instance, focus):
+        """
+        focus is if text field is being used, if it is being used focus = true,
+        but when user stops using it will be False, then we will fire validator for integers
+        and show an error if there will be one
+        """
+        if focus:
+            instance.text = ""
+            self.disable_checkboxes()
+        if not focus:
+            if not self.textfield_validator():
+                instance.text = "Tylko cyfry"
+                instance.error = True
+
+    def textfield_validator(self) -> bool:
+        fields = [self.ids.price_field_from, self.ids.price_field_to]
+        for field in fields:
+            try:
+                new_text = "%.2f" % float(field.text)
+            except ValueError:
+                return False
+            else:
+                field.text = new_text
+                return True
+
 
 # Tylko faktura
 OF3 = """
 <OdbiorcyFilter3>:
+    
     MDLabel:
         text: "Odbiorcy"
         size_hint: None, None
@@ -355,7 +435,7 @@ OF3 = """
         MDDropDownItemText:
             id: odbiorcy_drop
             text: "Wszyscy"
-            
+                
     MDButton:
         style: "text"
         pos_hint: {"x": 0.1, "y": 0.4}
@@ -369,12 +449,16 @@ OF3 = """
 
 
 # Tylko faktura
-class OdbiorcyFilter3(MDFloatLayout):
+class OdbiorcyFilter3(MDGridLayout):
 
     def __init__(self, **args):
         Builder.load_string(OF3)
         super().__init__()
+        self.cols = 1
+        self.row_default_height = 40
+
         self.size_hint = (1, .5)
+
         self.menu = None
 
     def open_menu(self, item):
@@ -516,18 +600,37 @@ class SklepyFilter3(MDFloatLayout):
         self.menu.dismiss()
 
 
-BAB3 = """
-<BotAppBar>:
-    MDFabBottomAppBarButton:
-        id: bottom_fab_app_bar
-        icon: "camera-plus-outline"
-        # on_release: app.bottom_menu_open()
-        on_release: app.swap_to_photoscreen1()
+BL3 = """
+<ButtonLayout3>:
+    MDButton:
+        id: clear
+        style: "text"
+        pos_hint: {"center_x": 0.25, "center_y": 0.5}
+        size_hint: 1, 1
+
+        MDButtonText: 
+            text: "WYCZYŚĆ"
+            pos_hint: {"center_x": 0.5, "center_y": 0.5}
+            size_hint: 1, 1
+            
+    MDButton:
+        id: apply
+        style: "text"
+        pos_hint: {"center_x": 0.75, "center_y": 0.5}
+        size_hint: 1, 1
+
+        MDButtonText: 
+            text: "ZASTOSUJ"
+            pos_hint: {"center_x": 0.5, "center_y": 0.5}
+            size_hint: 1, 1
+        
 """
 
 
-class BotAppBar(MDBottomAppBar):
+class ButtonLayout3(MDFloatLayout):
 
     def __init__(self, **args):
-        Builder.load_string(BAB3)
+        Builder.load_string(BL3)
         super().__init__()
+
+
